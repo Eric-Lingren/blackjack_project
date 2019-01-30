c3521cc8bf81bc531261397f32c7ae13ae7d3f78
Eric-Lingren <ericlingren@gmail.com>
2019-01-30 12:43:40 -0700

    fixing git ignore file

---
+++ build/precache-manifest.f9285ba7f66c1d70c7db9575356bd6a1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0a4e47fc533983b8613",
+    "url": "/Eric-Lingren/blackjack_project/static/css/main.883029d6.chunk.css"
+  },
+  {
+    "revision": "a0a4e47fc533983b8613",
+    "url": "/Eric-Lingren/blackjack_project/static/js/main.a0a4e47f.chunk.js"
+  },
+  {
+    "revision": "a707152af8360ec626cb",
+    "url": "/Eric-Lingren/blackjack_project/static/js/1.a707152a.chunk.js"
+  },
+  {
+    "revision": "88ce5bddbda03d3cb5f1",
+    "url": "/Eric-Lingren/blackjack_project/static/js/runtime~main.88ce5bdd.js"
+  },
+  {
+    "revision": "a7b2b6e3890e7883fcd8ef27f61ed4ac",
+    "url": "/Eric-Lingren/blackjack_project/static/media/blackjackLogo.a7b2b6e3.jpg"
+  },
+  {
+    "revision": "9695350fa10fd6908799f76d71eded82",
+    "url": "/Eric-Lingren/blackjack_project/static/media/$1.9695350f.png"
+  },
+  {
+    "revision": "8dc5f62d57eceb97a2c19e1f1e8b11e6",
+    "url": "/Eric-Lingren/blackjack_project/static/media/$5.8dc5f62d.png"
+  },
+  {
+    "revision": "b5a4a4371fdf2f29c5e3ce0631ab696b",
+    "url": "/Eric-Lingren/blackjack_project/static/media/$25.b5a4a437.png"
+  },
+  {
+    "revision": "09027d40dfc9a11929d1ac0f53ee7c29",
+    "url": "/Eric-Lingren/blackjack_project/static/media/$50.09027d40.png"
+  },
+  {
+    "revision": "a6391183cf0dddde4028c83162a559e7",
+    "url": "/Eric-Lingren/blackjack_project/static/media/$100.a6391183.png"
+  },
+  {
+    "revision": "0a3f4606e2c054e99d6527ec56dd7cdc",
+    "url": "/Eric-Lingren/blackjack_project/static/media/$500.0a3f4606.png"
+  },
+  {
+    "revision": "25b4a4d68d670438af9e4981c62f49f5",
+    "url": "/Eric-Lingren/blackjack_project/static/media/cardBack.25b4a4d6.png"
+  },
+  {
+    "revision": "3aaf2a5179aaf0e93583d7a5b20e0839",
+    "url": "/Eric-Lingren/blackjack_project/static/media/blackjackTable2.3aaf2a51.jpg"
+  },
+  {
+    "revision": "1cd2288f9d05ca6c6b86c6112841970f",
+    "url": "/Eric-Lingren/blackjack_project/index.html"
+  }
+];
